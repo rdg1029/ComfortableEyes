4aae0f963a45a88b1540c80fb5b612aed6493035
Caret <wcle3456@gmail.com>
2020-09-13 14:09:40 +0900

    Add EmergencyFragment : init

---
+++ app/src/main/java/com/comfortable/eyes/EmergencyFragment.java
@@ -0,0 +1,27 @@
+package com.comfortable.eyes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class EmergencyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_emergency, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public void emergencyButton(View v) {
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
+}
--- app/src/main/java/com/comfortable/eyes/MainActivity.java
+++ app/src/main/java/com/comfortable/eyes/MainActivity.java
@@ -8,14 +8,17 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private FragmentManager fragmentManager;
     private HomeFragment homeFragment;
     private SettingsFragment settingsFragment;
+    private EmergencyFragment emergencyFragment;
     private FragmentTransaction fragmentTransaction;
     private TextView menuName;
+    private ImageButton btnHome, btnSettings, btnEmergency;
 
     private void restartService() {
         ProtectModeState pmState = new ProtectModeState(this);
@@ -31,12 +34,19 @@ public class MainActivity extends AppCompatActivity {
         fragmentManager = getSupportFragmentManager();
         homeFragment = new HomeFragment();
         settingsFragment = new SettingsFragment();
+        emergencyFragment = new EmergencyFragment();
 
         fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.main_frameLayout, homeFragment).commitAllowingStateLoss();
 
         menuName = findViewById(R.id.main_menu_name);
         menuName.setText("오늘의 휴대폰 사용 시간");
+
+        btnHome = findViewById(R.id.main_btn_home);
+        btnSettings = findViewById(R.id.main_btn_settings);
+        btnEmergency = findViewById(R.id.main_btn_emergency);
+        btnSettings.setAlpha(0.5f);
+        btnEmergency.setAlpha(0.5f);
     }
 
     @Override
@@ -56,12 +66,25 @@ public class MainActivity extends AppCompatActivity {
             case R.id.main_btn_home:
                 menuName.setText("오늘의 휴대폰 사용 시간");
                 fragmentTransaction.replace(R.id.main_frameLayout, homeFragment).commitAllowingStateLoss();
+                btnHome.setAlpha(1.0f);
+                btnSettings.setAlpha(0.5f);
+                btnEmergency.setAlpha(0.5f);
                 break;
 
             case R.id.main_btn_settings:
                 menuName.setText("설정");
                 fragmentTransaction.replace(R.id.main_frameLayout, settingsFragment).commitAllowingStateLoss();
+                btnHome.setAlpha(0.5f);
+                btnSettings.setAlpha(1.0f);
+                btnEmergency.setAlpha(0.5f);
                 break;
+
+            case R.id.main_btn_emergency:
+                menuName.setText("문제 발생");
+                fragmentTransaction.replace(R.id.main_frameLayout, emergencyFragment).commitAllowingStateLoss();
+                btnHome.setAlpha(0.5f);
+                btnSettings.setAlpha(0.5f);
+                btnEmergency.setAlpha(1.0f);
         }
     }
 /*
