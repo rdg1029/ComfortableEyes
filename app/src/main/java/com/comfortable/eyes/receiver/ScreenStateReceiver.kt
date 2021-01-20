ba0382e1a5a5cef43b50f46af85231a3fe92fd64
rdg1029 <wcle3456@gmail.com>
2021-01-20 14:13:24 +0900

    Update ScreenStateReceiver : Add Action

--- app/src/main/java/com/comfortable/eyes/receiver/ScreenStateReceiver.kt
+++ app/src/main/java/com/comfortable/eyes/receiver/ScreenStateReceiver.kt
@@ -1,11 +1,37 @@
 package com.comfortable.eyes.receiver
 
+import android.app.KeyguardManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
 class ScreenStateReceiver: BroadcastReceiver() {
+    lateinit var mContext: Context
+    var isScreenLocked: Boolean = false
+
     override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("Not yet implemented")
+        if (intent == null || context == null) return
+
+        mContext = context
+
+        val km = mContext.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+
+        when(intent.action) {
+            Intent.ACTION_SCREEN_ON -> {
+                isScreenLocked = km.isKeyguardLocked
+            }
+
+            Intent.ACTION_SCREEN_OFF -> {
+                if (isScreenLocked) return
+            }
+
+            Intent.ACTION_USER_PRESENT -> {
+                isScreenLocked = km.isKeyguardLocked
+            }
+
+            Intent.ACTION_TIME_TICK -> {
+                if (km.isKeyguardLocked) return
+            }
+        }
     }
 }
