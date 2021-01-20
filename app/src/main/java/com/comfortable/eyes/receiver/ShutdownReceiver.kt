9c25d21a1a120344f6b4bafe9345d3466cbc416c
rdg1029 <wcle3456@gmail.com>
2021-01-20 14:52:22 +0900

    Add ShutdownReceiver : Init

---
+++ app/src/main/java/com/comfortable/eyes/receiver/ShutdownReceiver.kt
@@ -0,0 +1,11 @@
+package com.comfortable.eyes.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ShutdownReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
