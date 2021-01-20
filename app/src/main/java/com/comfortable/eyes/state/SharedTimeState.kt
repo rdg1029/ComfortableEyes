17ea816a8d03c4d96dd4a48bd7d9e91c0e37b544
rdg1029 <wcle3456@gmail.com>
2021-01-20 16:01:13 +0900

    Update SharedTimeState : Code Reorganize

--- app/src/main/java/com/comfortable/eyes/state/SharedTimeState.kt
+++ app/src/main/java/com/comfortable/eyes/state/SharedTimeState.kt
@@ -1,51 +1,36 @@
 package com.comfortable.eyes.state
 
 import android.content.Context
-import android.content.SharedPreferences
-import android.content.SharedPreferences.Editor
-import com.comfortable.eyes.Time
+import android.os.SystemClock
 
-class SharedTimeState(private val mContext: Context) {
-    private val pref: SharedPreferences
-    private val edit: Editor
-    private val time: Time
-    private val currentDate: String?
+class SharedTimeState(mContext: Context) {
+    private val pref = mContext.getSharedPreferences("SharedTimeState", 0)
+    private val edit = pref.edit()
+
+    var startTime: Long
+        get() = pref.getLong("time_start", 0)
+        set(value) {
+            edit.putLong("time_start", value)
+        }
+
+    var usedTime: Long
+        get() = pref.getLong("time_used", 0)
+        set(value) {
+            edit.putLong("time_used", value)
+        }
+
+    var dayOfYear: Int
+        get() = pref.getInt("time_day", 0)
+        set(value) {
+            edit.putInt("time_day", value)
+        }
+
+    fun init() {
+        edit.putLong("time_used", 0)
+        edit.putLong("time_start", SystemClock.elapsedRealtime())
+    }
+
     fun commitState() {
         edit.commit()
-    }
-
-    fun setTime(t: Time) {
-        edit.putInt("hour", t.hour)
-        edit.putInt("minutes", t.minutes)
-        edit.putInt("seconds", t.seconds)
-    }
-
-    fun setCurrentDate(d: String?) {
-        edit.putString("currentDate", d)
-    }
-
-    fun resetTime(t: Time) {
-        t.hour = 0
-        t.minutes = 0
-        t.seconds = 0
-        setTime(t)
-    }
-
-    fun getTime(): Time {
-        return time
-    }
-
-    fun getCurrentDate(): String? {
-        return currentDate
-    }
-
-    init {
-        time = Time()
-        pref = mContext.getSharedPreferences("SharedState", 0)
-        edit = pref.edit()
-        currentDate = pref.getString("currentDate", "0")
-        time.hour = pref.getInt("hour", 0)
-        time.minutes = pref.getInt("minutes", 0)
-        time.seconds = pref.getInt("seconds", 0)
     }
 }
