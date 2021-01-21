e443bd854c06abdd309f1120724d197a65a8db4b
rdg1029 <wcle3456@gmail.com>
2021-01-21 13:29:41 +0900

    Update RestartReceiver : Status Change By Date

--- app/src/main/java/com/comfortable/eyes/receiver/RestartReceiver.kt
+++ app/src/main/java/com/comfortable/eyes/receiver/RestartReceiver.kt
@@ -5,10 +5,21 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import com.comfortable.eyes.service.TimeCount
+import com.comfortable.eyes.state.SharedTimeState
+import java.util.*
 
 class RestartReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val i = Intent(context, TimeCount::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) context.startForegroundService(i) else context.startService(i)
+        val sharedTimeState = SharedTimeState(context)
+        if (sharedTimeState.dayOfYear != Calendar.getInstance().get(Calendar.DAY_OF_YEAR)) {
+            sharedTimeState.dayOfYear = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)
+            sharedTimeState.init()
+            sharedTimeState.commitState()
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            context.startForegroundService(i) 
+        else
+            context.startService(i)
     }
 }
