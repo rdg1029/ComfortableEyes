6a4ee18b317ff00613d5f1bbac47499666d098ca
rdg1029 <wcle3456@gmail.com>
2021-01-27 14:42:07 +0900

    Update RestActivity : Code reorganize

--- app/src/main/java/com/comfortable/eyes/activity/RestActivity.kt
+++ app/src/main/java/com/comfortable/eyes/activity/RestActivity.kt
@@ -3,11 +3,9 @@ package com.comfortable.eyes.activity
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
-import android.os.Build
-import android.os.Bundle
-import android.os.Handler
-import android.os.Message
+import android.os.*
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
@@ -18,19 +16,42 @@ import androidx.annotation.RequiresApi
 import com.comfortable.eyes.*
 import com.comfortable.eyes.service.RestModeCount
 import com.comfortable.eyes.service.TimeCount
-import com.comfortable.eyes.state.ProtectModeState
 import com.comfortable.eyes.state.RestModeState
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.InterstitialAd
 
 class RestActivity : Activity() {
-    private var pmState: ProtectModeState? = null
-    private var rmState: RestModeState? = null
-    private var rmTimer: TextView? = null
-    private var wording: TextView? = null
-    private var count = 0
-    private var finishButton: Button? = null
-    private var interstitialAd: InterstitialAd? = null
+    private lateinit var restModeState: RestModeState
+    private lateinit var restTimer: TextView
+    private lateinit var wording: TextView
+
+    private var startTime: Long = 0
+    private var endTime: Long = 0
+
+    private lateinit var finishButton: Button
+    private lateinit var interstitialAd: InterstitialAd
+
+    private val countRestTime: Handler = @SuppressLint("HandlerLeak")
+    object : Handler() {
+        @SuppressLint("SetTextI18n")
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+
+            setTimerText()
+
+            if (isCountFinished()) {
+                if (interstitialAd.isLoaded)
+                    interstitialAd.show()
+
+                wording.visibility = View.INVISIBLE
+                finishButton.visibility = View.VISIBLE
+            }
+            else {
+                this.sendEmptyMessageDelayed(0, 1000)
+            }
+        }
+    }
+
     private fun doFullScreen() {
         val decorView = window.decorView
         decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE or
@@ -39,6 +60,14 @@ class RestActivity : Activity() {
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_FULLSCREEN
+    }
+
+    private fun setTimerText() {
+        val sec = (endTime - SystemClock.elapsedRealtime()).toInt()
+        val m = (sec/60)%60
+        val s = sec%60
+
+        restTimer.text = "${if(m < 10) {"0$m"} else{"$m"}}:${if(s < 10) {"0$s"} else{"$s"}}"
     }
 
     //네비게이션 바, 상단바가 나왔을 때 화면을 터치하면 다시 전체화면으로 전환
@@ -53,113 +82,132 @@ class RestActivity : Activity() {
         val adDialog = AdDialog(this)
         adDialog.setTitle("휴식을 종료하시겠습니까?")
         adDialog.setPositiveButton {
-            rmState!!.isInterrupted = false
-            rmState!!.countValue = 0
-            count = 0
-            rmState!!.isActivityPaused = false
-            rmState!!.commitState()
+            endTime = SystemClock.elapsedRealtime()
+            restModeState.isRestPaused = false
+            restModeState.isInterrupted = false
+            restModeState.commitState()
+
             finish()
             adDialog.dismiss()
         }
         adDialog.setNegativeButton {
-            rmState!!.isInterrupted = false
-            rmState!!.isActivityPaused = false
-            rmState!!.commitState()
+            restModeState.isRestPaused = false
+            restModeState.isInterrupted = false
+            restModeState.commitState()
             adDialog.dismiss()
         }
         adDialog.show()
     }
 
-    @SuppressLint("HandlerLeak")
-    private val countRelaxingMode: Handler? = object : Handler() {
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
-            count = rmState!!.countValue
-            if (count == 0) {
-                if (interstitialAd!!.isLoaded) {
-                    interstitialAd!!.show()
-                }
-                wording!!.visibility = View.INVISIBLE
-                finishButton!!.visibility = View.VISIBLE
-            }
-            rmTimer!!.text = String.format("%s:%s", if (count / 60 < 10) "0" + count / 60 else Integer.toString(count / 60), if (count % 60 < 10) "0" + count % 60 else Integer.toString(count % 60))
-            this.sendEmptyMessageDelayed(0, 1000)
-        }
-    }
-
     private fun initInterstitialAd() {
         interstitialAd = InterstitialAd(this)
-        interstitialAd!!.adUnitId = "ca-app-pub-3940256099942544/1033173712" //전면 광고 TEST ID
-        interstitialAd!!.loadAd(AdRequest.Builder().build())
+        interstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712" //전면 광고 TEST ID
+        interstitialAd.loadAd(AdRequest.Builder().build())
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.i(this.javaClass.name, "onCreate 실행")
-        setContentView(R.layout.activity_rest)
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.cancel(3847)
-        pmState = ProtectModeState(this)
-        rmState = RestModeState(this)
-        rmTimer = findViewById(R.id.rest_count)
-        wording = findViewById(R.id.rest_wording)
-        finishButton = findViewById(R.id.rest_finish)
-        finishButton?.visibility = View.GONE
-        if (!pmState!!.isNotiCountPaused) {
-            finish()
-            return
-        }
-        initInterstitialAd()
-        doFullScreen()
-        stopService(Intent(this, TimeCount::class.java))
-        if (!rmState!!.isActivityPaused!!) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) startForegroundService(Intent(this, RestModeCount::class.java)) else startService(Intent(this, RestModeCount::class.java))
-        }
-        if (rmState!!.isInterrupted!!) {
-            Log.i(this.javaClass.name, "강제 중지 다이얼로그")
-            interruptedDialog()
-        } else {
-            rmState!!.isActivityPaused = false
-            rmState!!.commitState()
-        }
-        count = rmState!!.countValue
-        rmTimer?.text = String.format("%s:%s", if (count / 60 < 10) "0" + count / 60 else Integer.toString(count / 60), if (count % 60 < 10) "0" + count % 60 else Integer.toString(count % 60))
+    private fun isCountFinished(): Boolean {
+        return (endTime - SystemClock.elapsedRealtime() <= 0)
     }
 
     override fun onBackPressed() {}
+
     fun finishRelaxing(v: View?) {
-        count = rmState!!.countValue
-        if (count == 0) {
+        if (isCountFinished()) {
             finish()
         } else {
             Toast.makeText(applicationContext, "아직 종료할 수 없습니다", Toast.LENGTH_SHORT).show()
         }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i(this.javaClass.name, "onCreate 실행")
+        setContentView(R.layout.activity_rest)
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancel(3847)
+
+        restModeState = RestModeState(this)
+
+        if (!restModeState.isRestMode) {
+            finish()
+            return
+        }
+
+        restTimer = findViewById(R.id.rest_count)
+        wording = findViewById(R.id.rest_wording)
+        finishButton = findViewById(R.id.rest_finish)
+        finishButton.visibility = View.GONE
+
+        initInterstitialAd()
+        doFullScreen()
+        stopService(Intent(this, TimeCount::class.java))
+
+        if (!restModeState.isRestPaused) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                startForegroundService(Intent(this, RestModeCount::class.java))
+            else
+                startService(Intent(this, RestModeCount::class.java))
+
+            startTime = SystemClock.elapsedRealtime()
+            endTime = startTime + restModeState.restCount + 1000
+
+            restModeState.startTime = startTime
+            restModeState.endTime = endTime
+        }
+
+        if (restModeState.isInterrupted) {
+            Log.i(this.javaClass.name, "강제 중지 다이얼로그")
+            interruptedDialog()
+        } else {
+            restModeState.isRestPaused = false
+            restModeState.commitState()
+        }
+
+        setTimerText()
+    }
+
     override fun onResume() {
         super.onResume()
         Log.i(this.javaClass.name, "onResume 실행")
-        count = rmState!!.countValue
-        if (count > 0 && rmState!!.isActivityPaused!!) {
-            rmState!!.isActivityPaused = false
-            rmState!!.commitState()
+
+        if (!isCountFinished() && restModeState.isRestPaused) {
+            val continueTime = restModeState.restCount - (restModeState.pausedTime - restModeState.startTime).toInt()
+            startTime = SystemClock.elapsedRealtime()
+            endTime = startTime + continueTime
+
+            restModeState.startTime = startTime
+            restModeState.endTime = endTime
+
+            restModeState.isRestPaused = false
         }
-        if (countRelaxingMode != null) {
-            countRelaxingMode.removeMessages(0)
-            countRelaxingMode.sendEmptyMessage(0)
+        else {
+            startTime = SystemClock.elapsedRealtime()
+            endTime = startTime + restModeState.restCount + 1000
+
+            restModeState.startTime = startTime
+            restModeState.endTime = endTime
         }
+        restModeState.commitState()
+
+        countRestTime.removeMessages(0)
+        countRestTime.sendEmptyMessageDelayed(0, 0)
+
         doFullScreen()
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT_WATCH)
+    @RequiresApi(Build.VERSION_CODES.KITKAT_WATCH)
     override fun onPause() {
         super.onPause()
         Log.i(this.javaClass.name, "onPause 실행")
-        countRelaxingMode?.removeMessages(0)
-        val checkOnUsing = CheckOnUsing(this)
-        if (count > 0 && checkOnUsing.isScreenOn) {
-            rmState!!.isActivityPaused = true
-            rmState!!.commitState()
+
+        countRestTime.removeMessages(0)
+
+        val pm = getSystemService(Context.POWER_SERVICE) as PowerManager
+        if(!isCountFinished() && pm.isInteractive) {
+            restModeState.isRestPaused = true
+            restModeState.pausedTime = SystemClock.elapsedRealtime()
+            restModeState.commitState()
             finish()
         }
     }
@@ -167,19 +215,24 @@ class RestActivity : Activity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.i(this.javaClass.name, "onDestroy 실행")
-        if (!pmState!!.isNotiCountPaused) return
-        count = rmState!!.countValue
-        if (count > 0 && rmState!!.isActivityPaused!!) return
+        if (!isCountFinished() && restModeState.isRestPaused) return
+
         Log.i(this.javaClass.name, "onDestroy 실행(완전 종료)")
+
         stopService(Intent(this, RestModeCount::class.java))
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) startForegroundService(Intent(this, TimeCount::class.java)) else startService(Intent(this, TimeCount::class.java))
-        pmState!!.setNotiCountPause(false)
-        pmState!!.setNotUsingCountPause(false)
-        rmState!!.isActivityPaused = false
-        pmState!!.setNotiCount(pmState!!.notiTime)
-        pmState!!.setNotUsingCount(pmState!!.notiTime / 5)
-        pmState!!.commitState()
-        rmState!!.commitState()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            startForegroundService(Intent(this, TimeCount::class.java))
+        else
+            startService(Intent(this, TimeCount::class.java))
+
+        restModeState.isRestMode = false
+        restModeState.isRestPaused = false
+        restModeState.isInterrupted = false
+        restModeState.commitState()
+
+        val restAlarmManager = RestAlarmManager(this)
+        restAlarmManager.apply(restAlarmManager.timeAlarmCycle, restAlarmManager.timeRest, true)
+
         Toast.makeText(applicationContext, "휴식 모드 종료됨", Toast.LENGTH_SHORT).show()
     }
 }
