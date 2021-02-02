ca6368b3ff3c0871d53f93e718bf1ba16d044498
rdg1029 <wcle3456@gmail.com>
2021-02-02 16:05:12 +0900

    Update RestartReceiver : Set intent action

--- app/src/main/java/com/comfortable/eyes/receiver/RestartReceiver.kt
+++ app/src/main/java/com/comfortable/eyes/receiver/RestartReceiver.kt
@@ -19,33 +19,33 @@ class RestartReceiver : BroadcastReceiver() {
         val restAlarmManager = RestAlarmManager(context)
         val km = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
 
-        if (sharedTimeState.dayOfYear != Calendar.getInstance().get(Calendar.DAY_OF_YEAR)) {
-            sharedTimeState.dayOfYear = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)
-            sharedTimeState.init()
-            sharedTimeState.commitState()
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            if (sharedTimeState.dayOfYear != Calendar.getInstance().get(Calendar.DAY_OF_YEAR)) {
+                sharedTimeState.dayOfYear = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)
+                sharedTimeState.init()
+                sharedTimeState.commitState()
+            }
+
+            if (restAlarmManager.isAlarmEnabled && !km.isKeyguardLocked) {
+                val timeStart = SystemClock.elapsedRealtime()
+                val timeSleep = timeStart - restAlarmManager.timeAlarmCanceled
+                val timeAlarmActived = restAlarmManager.timeAlarmCanceled - restAlarmManager.timeAlarmApplied
+                val timeRest = restAlarmManager.timeRest
+
+                if (timeSleep < timeRest && timeSleep < timeAlarmActived) {
+                    Log.d("ScreenStateReceiver", "NOT Enough Rest")
+                    restAlarmManager.apply(restAlarmManager.timeAlarmCycle - (timeAlarmActived - timeSleep).toInt(), (timeRest - timeSleep).toInt(), false)
+                }
+                else {
+                    Log.d("ScreenStateReceiver", "Enough Rest")
+                    restAlarmManager.apply(restAlarmManager.timeAlarmCycle, restAlarmManager.timeRest, true)
+                }
+            }
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) 
             context.startForegroundService(i) 
         else 
             context.startService(i)
-
-        if (restAlarmManager.isAlarmEnabled) {
-            if (km.isKeyguardLocked) return
-
-            val timeStart = SystemClock.elapsedRealtime()
-            val timeSleep = timeStart - restAlarmManager.timeAlarmCanceled
-            val timeAlarmActived = restAlarmManager.timeAlarmCanceled - restAlarmManager.timeAlarmApplied
-            val timeRest = restAlarmManager.timeRest
-
-            if (timeSleep < timeRest && timeSleep < timeAlarmActived) {
-                Log.d("ScreenStateReceiver", "NOT Enough Rest")
-                restAlarmManager.apply(restAlarmManager.timeAlarmCycle - (timeAlarmActived - timeSleep).toInt(), (timeRest - timeSleep).toInt(), false)
-            }
-            else {
-                Log.d("ScreenStateReceiver", "Enough Rest")
-                restAlarmManager.apply(restAlarmManager.timeAlarmCycle, restAlarmManager.timeRest, true)
-            }
-        }
     }
 }
