b8e945ded65409f71992975894897fd306113762
rdg1029 <wcle3456@gmail.com>
2021-02-05 16:26:41 +0900

    Update AdDialog : Apply AdListener

--- app/src/main/java/com/comfortable/eyes/AdDialog.kt
+++ app/src/main/java/com/comfortable/eyes/AdDialog.kt
@@ -7,26 +7,43 @@ import android.view.Window
 import android.widget.Button
 import android.widget.TextView
 import com.google.android.ads.nativetemplates.TemplateView
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdLoader
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
 
 class AdDialog(private val mContext: Context) {
     private val dialog: Dialog = Dialog(mContext)
+    private lateinit var ad: TemplateView
+
+    private var isAdLoaded: Boolean = false
+
     fun setTitle(text: String?) {
         val title = dialog.findViewById<TextView>(R.id.ad_dialog_title)
         title.text = text
     }
 
     private fun setAdTemplate() {
-        val template: TemplateView = dialog.findViewById(R.id.ad_dialog_template)
-        template.visibility = View.INVISIBLE
-        val adBuilder = AdLoader.Builder(mContext, "ca-app-pub-3940256099942544/2247696110") //네이티브 광고 TEST ID
-        adBuilder.forUnifiedNativeAd { unifiedNativeAd ->
-            template.visibility = View.VISIBLE
-            template.setNativeAd(unifiedNativeAd)
+        ad = dialog.findViewById(R.id.ad_dialog_template)
+
+        //네이티브 광고 TEST ID
+        AdLoader.Builder(mContext, "ca-app-pub-3940256099942544/2247696110").apply {
+            forUnifiedNativeAd { unifiedNativeAd ->
+                ad.setNativeAd(unifiedNativeAd)
+            }
+            withAdListener(object : AdListener() {
+                override fun onAdFailedToLoad(p0: LoadAdError?) {
+                    ad.visibility = View.INVISIBLE
+                    isAdLoaded = false
+                }
+
+                override fun onAdLoaded() {
+                    ad.visibility = View.VISIBLE
+                    isAdLoaded = true
+                }
+            })
+            build().loadAd(AdRequest.Builder().build())
         }
-        val adLoader = adBuilder.build()
-        adLoader.loadAd(AdRequest.Builder().build())
     }
 
     fun setPositiveButton(listener: View.OnClickListener?) {
@@ -40,6 +57,8 @@ class AdDialog(private val mContext: Context) {
     }
 
     fun dismiss() {
+        if (isAdLoaded)
+            ad.destroyNativeAd()
         dialog.dismiss()
     }
 
