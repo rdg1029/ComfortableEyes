76e90635c2b4d63bdb782ee9ccf54ddff461e2da
rdg1029 <wcle3456@gmail.com>
2021-02-08 16:30:20 +0900

    Update NotiDialog : Change Notification Style

--- app/src/main/java/com/comfortable/eyes/NotiDialog.kt
+++ app/src/main/java/com/comfortable/eyes/NotiDialog.kt
@@ -7,28 +7,23 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import com.comfortable.eyes.receiver.NotiActionReceiver
 
 class NotiDialog(private val mContext: Context, private val dialogTextMSG: String, private val actionConfirm: String, private val actionCancel: String) {
     private var notiBuilder: NotificationCompat.Builder? = null
     private var notificationManager: NotificationManager? = null
+
+    private fun makeNotificationButton(action: String, btnTitle: String): NotificationCompat.Action {
+        val intent = Intent(mContext, NotiActionReceiver::class.java).let { i ->
+            i.action = action
+            PendingIntent.getBroadcast(mContext, 0, i, 0)
+        }
+
+        return NotificationCompat.Action.Builder(0, btnTitle, intent).build()
+    }
+
     private fun setNotification() {
-        val confirmIntent = Intent(mContext, NotiActionReceiver::class.java)
-        val cancelIntent = Intent(mContext, NotiActionReceiver::class.java)
-
-        confirmIntent.action = actionConfirm
-        cancelIntent.action = actionCancel
-
-        val confirmPIntent = PendingIntent.getBroadcast(mContext, 0, confirmIntent, 0)
-        val cancelPIntent = PendingIntent.getBroadcast(mContext, 0, cancelIntent, 0)
-
-        val remoteViews = RemoteViews(mContext.packageName, R.layout.noti_protect_mode)
-        remoteViews.setTextViewText(R.id.pm_dialog_msg, dialogTextMSG)
-        remoteViews.setOnClickPendingIntent(R.id.pm_dialog_btn_confirm, confirmPIntent)
-        remoteViews.setOnClickPendingIntent(R.id.pm_dialog_btn_cancel, cancelPIntent)
-
         notificationManager = mContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel("RestAlarm", "RestAlarm", NotificationManager.IMPORTANCE_HIGH)
@@ -39,10 +34,13 @@ class NotiDialog(private val mContext: Context, private val dialogTextMSG: Strin
         }
         notiBuilder = NotificationCompat.Builder(mContext, "RestAlarm")
                 .setSmallIcon(R.drawable.ic_baseline_visibility_24)
-                .setContent(remoteViews)
+                .setContentTitle("쉬는 시간")
+                .setContentText(dialogTextMSG)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setDefaults(Notification.DEFAULT_VIBRATE)
                 .setAutoCancel(false)
+                .addAction(makeNotificationButton(actionConfirm, "확인"))
+                .addAction(makeNotificationButton(actionCancel, "취소"))
     }
 
     fun buildNotification(): Notification {
