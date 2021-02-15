ef42cdfe4321066b146d6bed2e75d119209779b0
rdg1029 <wcle3456@gmail.com>
2021-02-15 17:24:17 +0900

    Update TimeNotification : Change class to object

--- app/src/main/java/com/comfortable/eyes/TimeNotification.kt
+++ app/src/main/java/com/comfortable/eyes/TimeNotification.kt
@@ -4,16 +4,14 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
-import android.content.Context
 import android.graphics.Color
 import android.os.Build
 import android.os.SystemClock
 import androidx.core.app.NotificationCompat
 import com.comfortable.eyes.state.SharedTimeState
+import com.comfortable.eyes.AppContext.Companion.context
 
-class TimeNotification(context: Context) {
-    private val mContext = context
-
+object TimeNotification {
     var usedTimeToMin: String = ""
         get() {
             val sec = (( SharedTimeState.usedTime + (SystemClock.elapsedRealtime() - SharedTimeState.startTime) ) / 1000).toInt()
@@ -33,14 +31,14 @@ class TimeNotification(context: Context) {
         }
 
     fun buildNotification(contentText: String): Notification? {
-        val notificationManager = mContext.getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager = context.getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel("TimeCount", "UsingTime", NotificationManager.IMPORTANCE_LOW)
             notificationChannel.vibrationPattern = longArrayOf(0)
             notificationChannel.enableVibration(true)
             notificationManager.createNotificationChannel(notificationChannel)
         }
-        return NotificationCompat.Builder(mContext, "TimeCount")
+        return NotificationCompat.Builder(context, "TimeCount")
                 .setSmallIcon(R.drawable.ic_baseline_visibility_24)
                 .setContentTitle("휴대폰 사용 시간")
                 .setContentText(contentText)
@@ -52,7 +50,7 @@ class TimeNotification(context: Context) {
 
     fun updateNotification() {
         val notification = buildNotification(this.usedTimeToMin)
-        val notiManager = mContext.getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
+        val notiManager = context.getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
         notiManager.notify(1029, notification)
     }
 }
