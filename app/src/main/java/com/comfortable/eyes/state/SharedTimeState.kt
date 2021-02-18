5eb763b8582d205e24d818be56982219f8b57999
rdg1029 <wcle3456@gmail.com>
2021-02-18 16:34:21 +0900

    Update SharedTimeState : Remove function

--- app/src/main/java/com/comfortable/eyes/state/SharedTimeState.kt
+++ app/src/main/java/com/comfortable/eyes/state/SharedTimeState.kt
@@ -8,20 +8,12 @@ object SharedTimeState {
     private val edit = pref.edit()
 
     var startTime: Long = 0
-    var usedTime: Long = 0
-    var dayOfYear: Int = 0
+    var usedTime: Long = pref.getLong("time_used", 0)
+    var dayOfYear: Int = pref.getInt("day_of_year", 0)
 
 //    fun getSavedStartTime() {
 //        startTime = pref.getLong("time_start", 0)
 //    }
-
-    fun getSavedUsedTime() {
-        usedTime = pref.getLong("time_used", 0)
-    }
-
-    fun getSavedDayOfYear() {
-        dayOfYear = pref.getInt("day_of_year", 0)
-    }
 
     fun init() {
         usedTime = 0
